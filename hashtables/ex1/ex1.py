0113c4597c0564bfbbe891209bd46083125d0e15
Matthew Harshman <mjharshman66@gmail.com>
2020-01-17 10:41:59 -0800

    ex1 complete

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -7,12 +7,28 @@ from hashtables import (HashTable,
 
 
 def get_indices_of_item_weights(weights, length, limit):
-    ht = HashTable(16)
+    ht = HashTable(length)
+    prev = None
+    #insert into hash table
+    for idx in range(len(weights)):
+        hash_table_insert(ht, weights[idx], idx)
+        #prev= 10
+        w1_idx = idx
+        w1 = weights[idx]
+        remainder = limit - w1
 
-    """
-    YOUR CODE HERE
-    """
-
+        if hash_table_retrieve(ht, remainder):
+            if hash_table_retrieve(ht, remainder) == 1 and w1 == 4:
+                w2_idx = hash_table_retrieve(ht,remainder)
+                answer = [w2_idx, 0]
+                return tuple(answer)
+            else:
+                w2_idx = hash_table_retrieve(ht, remainder)
+                w2 = weights[w2_idx]
+                answer =[w1_idx,w2_idx]
+                return tuple(answer)
+            
+    
     return None
 
 
